3e6440df4277bf5fb439b3de20c0d431f3584847
Jonathan Dingess <J_And_M33@yahoo.com>
2018-02-25 16:01:13 -0500

    display

--- manage.py
+++ manage.py
@@ -19,6 +19,12 @@ def main():
 					user = ui[1]
 				else:
 					print('invalid username or password')
+					
+		if ui[0] == 'display':
+			if len(ui) < 2:
+				print('>>> USAGE: display <tablename> <<<')
+			else:
+				display(ui[1])
 
 
 
@@ -38,6 +44,23 @@ def login(username, password):
 
 	return False 
 
+def display(tablename):
+	with open(tablename+'.csv', 'r') as f:
+		table = [l.rstrip().split(',') for l in f.readlines()]
+	
+	#Get sizes of each input for display
+	maxlen = [max([len(i[j]) for i in table])+2 for j in range(len(table[0]))]
+
+	print('='*sum(maxlen))
+	for j in range(len(table[0])):
+		print(table[0][j]+(' '*(maxlen[j]-len(table[0][j]))), sep='', end='')
+	print()
+	print('='*sum(maxlen))
+	for i in range(1,len(table)):
+		for j in range(len(table[i])):
+			print(table[i][j]+(' '*(maxlen[j]-len(table[i][j]))), sep='', end='')
+		print()
+		
 
 def grant():
 	pass
