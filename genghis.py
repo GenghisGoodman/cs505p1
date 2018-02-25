1519e0e310ac6032fa7bcc747890dcfa4780d4ec
gpgo222 <genghis.goodman@uky.edu>
2018-02-25 17:51:31 -0500

    implemented authentication

--- genghis.py
+++ genghis.py
@@ -1,9 +1,14 @@
+from collections import defaultdict
+
 def create(user, ui):
 	if len(ui) != 2:
 		print('USAGE: CREATE <tablename>')
+		return
+	with open('grantLog.csv','a') as f:
+		f.write(','.join(['admin',user,ui[1],'1','+\n']))
 
 	with open('assigned.csv', 'a') as f:
-		f.write('admin' + ',' + ui[1] + ',' +'1' + '\n')
+		f.write('admin,' + ui[1] + ',' +'1' + '\n')
 		if user != '':
 			f.write(user + ',' + ui[1] + ',' +'1' + '\n')
 
@@ -65,3 +70,38 @@ def register(ui):
 
 	with open('login.csv', 'a') as f:
 		f.write(','.join(ui[1:])+'\n')
+
+def authenticate(user, fileName, permission):
+
+	usergraph = defaultdict(list)
+	with open('grantLog.csv', 'r') as f:
+		line = f.readline()
+		while line != '':
+			u1, u2, fn, b, p = line.rstrip().split(',')
+			if fn == fileName and (p == permission or p == '+'):
+				usergraph[u1].append([u2, b])
+			line = f.readline()
+
+	stack = ['admin']
+	seen = defaultdict(int)
+	current = ''
+	while current != user and len(stack) > 0:
+		current = stack.pop()
+		for node in usergraph[current]:
+			u,b = node 
+			if not seen[u] and int(b):
+				stack.append(u)
+
+			elif not seen[u]:
+				if u == user:
+					current = u
+					break
+		seen[current] = 1
+
+	return current == user
+
+
+
+
+
+
